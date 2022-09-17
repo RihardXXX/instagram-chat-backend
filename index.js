b7db937d0d8bffd5eb0720699781e865d4d5c9bd
RihardXXX <yusupa.akaeva@yandex.ru>
2022-09-17 19:53:23 +0300

    initial backend

---
+++ index.js
@@ -0,0 +1,6 @@
+// Подключаем бэкенд на Express.
+const express = require('express');
+
+// создаём приложение и настраиваем сокеты
+const app = require('express')();
+const server = require('http').createServer(app);
